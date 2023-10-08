3e76eea6b32647437a7a148a5275f58a1a9061b9
Eklavya Agarwal <eklavyaagarwal1810@gmail.com>
2023-10-08 17:59:00 +0800

    logger complete

---
+++ async_logger/log_component.py
@@ -0,0 +1,79 @@
+import os
+import datetime
+import asyncio
+from async_logger.ilog import ILog
+
+
+class LogComponent(ILog):
+    def __init__(self):
+        self.current_log_file = None
+        self.is_running = True
+        self.queue = asyncio.Queue()
+        now = datetime.datetime.now()
+        self.last_date = now.strftime('%Y_%m_%d')
+<<<<<<< HEAD
+        os.makedirs('logs', exist_ok= True)
+=======
+        os.makedirs('logs',exist_ok=True)
+>>>>>>> 540f7ee6e928d57cadad95a247bba428bea12605
+        self.last_filename = f"logs/{now.strftime('%Y_%m_%d_%H_%M_%S')}.txt"
+
+
+    def write(self, message: str) -> None:
+       self.queue.put_nowait(message)
+
+    async def stop(self, immediate: bool = False) -> None:
+        self.is_running = False
+
+        if not immediate:
+            await self.wait_for_completion()
+
+    async def wait_for_completion(self) -> None:
+        await self.queue.join()
+
+
+    async def _create_new_log_file(self) -> None:
+        now = datetime.datetime.now()
+        filename = f"log_{now.strftime('%Y_%m_%d_%H_%M_%S')}.txt"
+        self.current_log_file = filename
+
+        # Create a new log file
+        with open(filename, 'w') as log_file:
+            log_file.write(f"Log file created at {now}\n")
+
+    async def handle(self, message):
+        now = datetime.datetime.now()
+        date_prefix = now.strftime('%Y_%m_%d')
+        if date_prefix != self.last_date:
+            self.last_date = date_prefix
+            self.last_filename = f"logs/{now.strftime('%Y_%m_%d_%H_%M_%S')}.txt"
+
+        try:
+            with open(self.last_filename, 'a') as log_file:
+                log_file.write(message + '\n')
+        finally:
+            self.queue.task_done()
+
+    async def _process_queue(self) -> None:
+        print('processing')
+        while self.is_running or not self.queue.empty():
+            message = await self.queue.get()
+
+<<<<<<< HEAD
+            await self.handle(message)
+=======
+            # await self.handle(message)
+            
+
+            now = datetime.datetime.now()
+            date_prefix = now.strftime('%Y_%m_%d')
+            if date_prefix != self.last_date:
+                self.last_date = date_prefix
+                self.last_filename = f"logs/{now.strftime('%Y_%m_%d_%H_%M_%S')}.txt"
+
+            try:
+                with open(self.last_filename, 'a') as log_file:
+                    log_file.write(message + '\n')
+            finally:
+                self.queue.task_done()
+>>>>>>> 540f7ee6e928d57cadad95a247bba428bea12605
